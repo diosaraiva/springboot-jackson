28cbc96d384cf04ad96d485192e908ace11ef849
diosaraiva <diosaraiva@192.168.0.13>
2024-03-09 17:11:22 -0300

    new repo.

---
+++ src/main/java/com/diosaraiva/jackson/DataLoader.java
@@ -0,0 +1,14 @@
+package com.diosaraiva.jackson;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataLoader implements ApplicationRunner {
+
+	public void run(ApplicationArguments args) {
+
+		//
+	}
+}
